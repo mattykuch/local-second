aebb629dfd4caf6820072c1378ea25f1c83f1fdd
Matthew Kuch <mattykuchug@gmail.com>
2023-01-15 19:49:22 +0300

    Baseplot of penguins data

--- Scatter_penguins.R
+++ Scatter_penguins.R
@@ -2,3 +2,12 @@ library(ggplot2)
 library(palmerpenguins)
 
 #base plot 
+
+base_plot <- 
+  ggplot(data = penguins, aes(x = flipper_length_mm, y= body_mass_g)) +
+  geom_point(aes(color = species,
+                 shape = species), 
+             size = 2) +
+  scale_color_manual(values = c("darkorange","darkorchid","cyan4"))
+
+base_plot
