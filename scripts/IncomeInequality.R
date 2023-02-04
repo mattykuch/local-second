c9f974a39e6d0400e1ec677bb8446b23dd3b2e3c
Matthew Kuch <mattykuchug@gmail.com>
2023-02-04 19:56:59 +0300

    update to inequality script

--- scripts/IncomeInequality.R
+++ scripts/IncomeInequality.R
@@ -5,7 +5,7 @@
 
 install.packages("tidytuesdayR")
 
-#Other libraries
+#libraries
 library(tidyverse)
 library(tidytuesdayR)
 library(scales)
@@ -19,7 +19,33 @@ library(scales)
 
 tuesdata <- tidytuesdayR::tt_load(2021, week = 7)
 
+#Testing out the dataset
 tuesdata
-tuesdata$income_time
+tuesdata$lifetime_earn
 
-lifetime_earn <- tuesdata$lifetime_earn
+#Make one chart for each dataset to get a feel for what is possible
+
+#Dataset 1 - Average Life time earnings by Race and Gender
+
+lifetime_earn_tbl <- tuesdata$lifetime_earn
+
+lifetime_earn_tbl %>% ggplot(aes(lifetime_earn, race, fill=gender)) + 
+                      geom_col(position = "dodge") +
+                      scale_x_continuous(labels = dollar)
+#Dataset 2 - Student Debt
+tuesdata$student_debt -> student_debt_tbl
+
+#Chart 2.1
+student_debt_tbl %>% ggplot(aes(year, loan_debt_pct, color=race )) +
+                    geom_line() +
+                    expand_limits(y=0) +
+                    scale_y_continuous(labels = percent) +
+                    labs(y = "% of families with student loan debt")
+#Chart 2.2
+
+student_debt_tbl %>% ggplot(aes(year, loan_debt, color=race )) +
+  geom_line() +
+  expand_limits(y=0) +
+  scale_y_continuous(labels = dollar) +
+  labs(y = "Average loan debt")
+
