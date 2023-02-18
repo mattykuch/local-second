de7ca22f35f0880d1130122daab683d6b172a99e
Matthew Kuch <mattykuchug@gmail.com>
2023-02-19 00:35:24 +0300

    Axes and Text labels formatted

--- scripts/NeatAxes.R
+++ scripts/NeatAxes.R
@@ -9,4 +9,11 @@ base_plot <- ggplot(data = penguins, aes(x=flipper_length_mm, y=body_mass_g)) +
   geom_point(aes(color = species, shape = species), size=2) +
   scale_color_manual(values = c("darkorange", "darkorchid", "cyan4"))
 
-base_plot
+
+
+#Styles, axes, grids
+
+base_plot +
+  scale_x_continuous(limits = c(170,240), 
+                     labels = scales::label_number(suffix = "mm")) +
+  scale_y_continuous(labels = scales::label_comma(suffix = "g"))
