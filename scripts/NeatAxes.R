4afa7b70fcdfe7c2ec365bbf64456aa72596bf55
Matthew Kuch <mattykuchug@gmail.com>
2023-02-19 17:04:25 +0300

    Grid and axis line changes

--- scripts/NeatAxes.R
+++ scripts/NeatAxes.R
@@ -16,4 +16,12 @@ base_plot <- ggplot(data = penguins, aes(x=flipper_length_mm, y=body_mass_g)) +
 base_plot +
   scale_x_continuous(limits = c(170,240), 
                      labels = scales::label_number(suffix = "mm")) +
-  scale_y_continuous(labels = scales::label_comma(suffix = "g"))
+  scale_y_continuous(labels = scales::label_comma(suffix = "g")) + 
+  theme(
+    panel.background = element_blank(),
+    panel.grid.major = element_line(color = "#e7e7e7",size = 0.4),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "#e7e7e7",size = 0.8,
+                             arrow = arrow(length = unit(2, "mm")))
+    
+  )
