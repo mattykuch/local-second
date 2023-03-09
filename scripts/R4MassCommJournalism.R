6717e5166d67d7a68c11bbd8d9d20f0f216d9230
Matthew Kuch <mattykuchug@gmail.com>
2023-03-09 21:14:37 +0300

    comparing median household income in us vs 2 states : ca and ma

--- scripts/R4MassCommJournalism.R
+++ scripts/R4MassCommJournalism.R
@@ -2,4 +2,32 @@
 library(quantmod)
 library(dygraphs)
 
-#3.4 Download and graph a city’s median income
+#3.4 
+
+#Graph a MA’s median income
+
+ma_income <- getSymbols("MEHOINUSMAA646N", src="FRED", auto.assign = FALSE)
+
+names(ma_income) <- "MA Income"
+dygraph(ma_income, main = "Massachusetts Median Household Income")
+
+#Graph a CA’s median income
+
+ca_income <- getSymbols("MEHOINUSCAA646N", src="FRED", auto.assign = FALSE)
+
+names(ca_income) <- "CA Income"
+dygraph(ca_income, main = "California Median Household Income")
+
+#Graph a US’s median income
+
+us_income <- getSymbols("MHIUS00000A052NCEN", src="FRED", auto.assign = FALSE)
+
+names(us_income) <- "US Income"
+dygraph(us_income, main = "United States Median Household Income")
+
+#Combine charts
+mygraphdata <- cbind(ma_income,ca_income,us_income)
+
+dygraph(mygraphdata, main = "US, MA and CA Median Household Income")
+
+
